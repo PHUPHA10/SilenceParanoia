86eee974592f1a805ae196b59fce637b5e868612
John55521 <65111534@Dpu.ac.th>
2025-11-24 15:01:21 +0700

    StartEnemy
    1 percent

---
+++ Assets/Scripts/AllEnemy/EnemyAi.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    public NavMeshAgent ai;
+    public List<Transform> destinations;
+    public Animator aiAnime;
+
+
+}
--- Assets/Scripts/Storage/ItemDefinition1.cs
+++ Assets/Scripts/Storage/ItemDefinition1.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class ItemDefinition : ScriptableObject
 {
     public string displayName;
-    public Sprite icon;               // ไม่ได้โชว์ แต่เก็บไว้ใช้ภายหลัง
+    public Sprite icon;              
     public int maxStack = 1;
     [Header("Optional - show in hand")]
-    public GameObject heldPrefab;     // ถ้าอยากให้ถือ
+    public GameObject heldPrefab;     
     public Vector3 heldLocalPosition = new Vector3(0.38f, -0.38f, 0.72f);
     public Vector3 heldLocalEuler    = new Vector3(8f, 0f, 0f);
     public Vector3 heldLocalScale    = Vector3.one;
