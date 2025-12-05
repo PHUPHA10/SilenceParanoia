b3646ec4737dc8f1faaeea88897c220e819ad559
John55521 <65111534@Dpu.ac.th>
2025-12-05 13:51:36 +0700

    Continue
    Fix chat & Enemy Ai Nav

--- Assets/Scripts/AllEnemy/EnemyAi.cs
+++ Assets/Scripts/AllEnemy/EnemyAi.cs
@@ -1,13 +1,86 @@
-using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAi : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
     public NavMeshAgent ai;
     public List<Transform> destinations;
-    public Animator aiAnime;
 
+    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idleTime, sightDistance, catchDistance;
+    public bool walking, chasing;
+    public Transform player;
 
+    Transform currentDest;
+    Vector3 dest;
+
+    int randNum;
+
+    public Vector3 rayCastOffset;
+
+    void Start()
+    {
+        walking = true;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+    }
+
+    void Update()
+    {
+        // ????????????????????????????
+        Vector3 direction = (player.position - transform.position).normalized;
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position + rayCastOffset, direction, out hit, sightDistance))
+        {
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                walking = false;
+                StopCoroutine("stayIdle");
+                chasing = true;
+            }
+        }
+
+        // ??????????
+        if (chasing)
+        {
+            dest = player.position;
+            ai.destination = dest;
+            ai.speed = chaseSpeed;
+
+            float distance = Vector3.Distance(player.position, ai.transform.position);
+            if (distance <= catchDistance)
+            {
+                Debug.Log("Player caught!"); // ?????????
+                chasing = false;
+            }
+        }
+
+        // ??????????????
+        if (walking)
+        {
+            dest = currentDest.position;
+            ai.destination = dest;
+            ai.speed = walkSpeed;
+
+            if (ai.remainingDistance <= ai.stoppingDistance)
+            {
+                ai.speed = 0;
+                StopCoroutine("stayIdle");
+                StartCoroutine(stayIdle());
+                walking = false;
+            }
+        }
+    }
+
+    IEnumerator stayIdle()
+    {
+        idleTime = Random.Range(minIdleTime, maxIdleTime);
+        yield return new WaitForSeconds(idleTime);
+
+        walking = true;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+    }
 }
